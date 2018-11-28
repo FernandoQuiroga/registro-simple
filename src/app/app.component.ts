dd8e31aced968ec413c913c03667c96c73f359fe
LuisFer Quiroga <lfquirogaa@gmail.com>
2018-11-28 08:42:50 -0400

    registro angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Alumno } from "./models/alumno";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'registro-angular-simple';
+  alumnoArray: Alumno[] = [
+    { id: 1, nombre_completo: "Luis Quiroga", identidad: "8426551" },
+    { id: 2, nombre_completo: "Fernando Altamirano", identidad: "1558469" },
+    { id: 3, nombre_completo: "Pepito Grillo", identidad: "6666666" }
+  ];
+  selectedAlumno: Alumno = { id: 0, nombre_completo: "", identidad: "" };
+  openForEdit(Alumno: Alumno): void {
+    this.selectedAlumno = Alumno;
+  }
+  addOrEdit(): void {
+    if (this.selectedAlumno.id === 0) {
+      this.selectedAlumno.id = this.alumnoArray.length + 1;
+      this.alumnoArray.push(this.selectedAlumno);
+    }
+    this.selectedAlumno = { id: 0, nombre_completo: "", identidad: "" };
+  }
+  delete(): void {
+    if (confirm("esta seguro de eliminar esto?")) {
+      this.alumnoArray = this.alumnoArray.filter(x => x != this.selectedAlumno);
+      this.selectedAlumno = { id:0, nombre_completo: "", identidad: "" };
+    }
+  }
 }
